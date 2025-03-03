35e1ac2a04f7e3feed64a38dc6011bc4983b2cb1
Vergueirod <diego.ssv@outlook.com>
2025-03-03 13:06:17 -0300

    Initial commit

---
+++ src/main/java/com/vergueiro_group/notifuhub_backend_taskScheduler/NotifuhubBackendTaskSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.vergueiro_group.notifuhub_backend_taskScheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifuhubBackendTaskSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifuhubBackendTaskSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vergueiro_group/notifuhub_backend_taskScheduler/NotifuhubBackendTaskSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vergueiro_group.notifuhub_backend_taskScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifuhubBackendTaskSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
